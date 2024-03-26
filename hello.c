b1f229fdf640dc0385bf635f55859ff14d170a86
Alan <alan.knight@me.com>
2024-03-26 22:39:14 +0000

    Update hello.c

--- hello.c
+++ hello.c
@@ -4,6 +4,11 @@
 int main(int argc, char **argv)
 {
     printf("Hello, World!\n%04d\n", argc);
+    if(argc > 1)
+        for(i=1, i < argc, i++)
+            {
+                printf("%02d %s\n", i, argv[i]);
+            }
     exit(0);
 }
     
